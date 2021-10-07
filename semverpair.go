d94c8ff05b2983c0a94359d260094e14ff52844b
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-10-07 19:22:30 +0200

    Add some version structs

--- semverpair.go
+++ semverpair.go
@@ -2,35 +2,56 @@ package semverpair
 
 import "fmt"
 
-// EncodeSemverPair encodes two semver versions that share the same major version
+// Version represents a semver version.
+type Version struct {
+	Major int
+	Minor int
+	Patch int
+}
+
+// Pairs represents a pair of semver version sharing the same major version.
+type Pair struct {
+	First  Version
+	Second Version
+}
+
+// String formats the version triplet as a semver string, e.g. v3.5.2.
+func (v Version) String() string {
+	return fmt.Sprintf("v%d.%d.%d", v.Major, v.Minor, v.Patch)
+}
+
+// Encode encodes two semver versions that share the same major version
 // into one.
 // The encoded version can be decoded back to the originals using DecodeSemver
 // as long as minor2 and patch2 is less than 100.
-func EncodeSemverPair(major1, minor1, patch1, minor2, patch2 int) (int, int, int) {
-	return major1, (minor1 * 100) + minor2, (patch1 * 100) + patch2
+func Encode(pair Pair) Version {
+	return Version{
+		Major: pair.First.Major,
+		Minor: (pair.First.Minor * 100) + pair.Second.Minor,
+		Patch: (pair.First.Patch * 100) + pair.Second.Patch,
+	}
 }
 
-// DecodeSemver decodes the given semver triplet into the original
+// Decode decodes the given semver triplet into the original
 // version pair.
 // This only works if the original pair's second version's minor and patch version
 // was less than 100.
-func DecodeSemver(major, minor, patch int) (int, int, int, int, int) {
-	minor1 := (minor / 100)
-	patch1 := (patch / 100)
-	minor2 := minor - (minor1 * 100)
-	patch2 := patch - (patch1 * 100)
-	return major, minor1, patch1, minor2, patch2
-}
+func Decode(v Version) Pair {
+	minor1 := (v.Minor / 100)
+	patch1 := (v.Patch / 100)
+	minor2 := v.Minor - (minor1 * 100)
+	patch2 := v.Patch - (patch1 * 100)
 
-// DecodeFirstSemver utility function to decode the first triplet, e.g.
-// the upstream version.
-func DecodeFirstSemver(major, minor, patch int) (int, int, int) {
-	major, minor, patch, _, _ = DecodeSemver(major, minor, patch)
-	return major, minor, patch
-}
-
-// SemverString is just a helper that formats the version triplet as
-// a semver string, e.g. v3.5.2.
-func SemverString(major, minor, patch int) string {
-	return fmt.Sprintf("v%d.%d.%d", major, minor, patch)
+	return Pair{
+		First: Version{
+			Major: v.Major,
+			Minor: minor1,
+			Patch: patch1,
+		},
+		Second: Version{
+			Major: v.Major,
+			Minor: minor2,
+			Patch: patch2,
+		},
+	}
 }
--- semverpair_test.go
+++ semverpair_test.go
@@ -17,22 +17,19 @@ func TestSemverPair(t *testing.T) {
 		v2     string
 		expect string
 	}{
-		{"v3.6.5", "v0.3.2", "v3.603.502"},
-		{"v3.88.43", "v0.3.2", "v3.8803.4302"},
-		{"v3.6.5", "v0.13.24", "v3.613.524"},
-		{"v3.6.5", "v0.99.99", "v3.699.599"},
-		{"v3.88.43", "v0.13.24", "v3.8813.4324"},
+		{"v3.6.5", "v3.3.2", "v3.603.502"},
+		{"v3.88.43", "v3.3.2", "v3.8803.4302"},
+		{"v3.6.5", "v3.13.24", "v3.613.524"},
+		{"v3.6.5", "v3.99.99", "v3.699.599"},
+		{"v3.88.43", "v3.13.24", "v3.8813.4324"},
 	} {
-		major, minor1, patch1 := splitSemver(test.v1)
-		_, minor2, patch2 := splitSemver(test.v2)
-		encMajor, encMinor, encPatch := EncodeSemverPair(major, minor1, patch1, minor2, patch2)
-		encoded := SemverString(encMajor, encMinor, encPatch)
-		c.Assert(encoded, qt.Equals, test.expect)
-		c.Assert(semver.Compare(test.v1, encoded), qt.Equals, -1)
-		c.Assert(SemverString(DecodeFirstSemver(encMajor, encMinor, encPatch)), qt.Equals, test.v1)
-		_, _, _, minor2Decoded, patch2Decoded := DecodeSemver(encMajor, encMinor, encPatch)
-		c.Assert(minor2Decoded, qt.Equals, minor2)
-		c.Assert(patch2Decoded, qt.Equals, patch2)
+		v1, v2 := splitSemver(test.v1), splitSemver(test.v2)
+		encoded := Encode(Pair{First: v1, Second: v2})
+		c.Assert(encoded.String(), qt.Equals, test.expect)
+		c.Assert(semver.Compare(test.v1, encoded.String()), qt.Equals, -1)
+		decoded := Decode(encoded)
+		c.Assert(decoded.First.String(), qt.Equals, test.v1)
+		c.Assert(decoded.Second, qt.Equals, v2)
 
 	}
 }
@@ -46,34 +43,36 @@ func TestSemverPairOverflow(t *testing.T) {
 		expect    string
 		overflows bool
 	}{
-		{"v3.632.5", "v0.3.2", "v3.63203.502", false},
-		{"v3.6.5", "v0.100.2", "v3.700.502", true},
-		{"v3.6.5", "v0.3.100", "v3.603.600", true},
-		{"v3.1234.5", "v0.3.2", "v3.123403.502", false},
-		{"v3.6.12345", "v0.3.2", "v3.603.1234502", false},
+		{"v3.632.5", "v3.3.2", "v3.63203.502", false},
+		{"v3.6.5", "v3.100.2", "v3.700.502", true},
+		{"v3.6.5", "v3.3.100", "v3.603.600", true},
+		{"v3.1234.5", "3.3.2", "v3.123403.502", false},
+		{"v3.6.12345", "v3.3.2", "v3.603.1234502", false},
 	} {
-		major, minor1, patch1 := splitSemver(test.v1)
-		_, minor2, patch2 := splitSemver(test.v2)
-		encMajor, encMinor, encPatch := EncodeSemverPair(major, minor1, patch1, minor2, patch2)
-		encoded := SemverString(encMajor, encMinor, encPatch)
-		c.Assert(encoded, qt.Equals, test.expect)
-		c.Assert(semver.Compare(test.v1, encoded), qt.Equals, -1)
+		v1, v2 := splitSemver(test.v1), splitSemver(test.v2)
+		encoded := Encode(Pair{First: v1, Second: v2})
+		c.Assert(encoded.String(), qt.Equals, test.expect)
+		c.Assert(semver.Compare(test.v1, encoded.String()), qt.Equals, -1)
 
 		checker := qt.Equals
 		if test.overflows {
 			checker = qt.Not(qt.Equals)
 		}
 
-		c.Assert(SemverString(DecodeFirstSemver(encMajor, encMinor, encPatch)), checker, test.v1)
+		c.Assert(Decode(encoded).First.String(), checker, test.v1)
 
 	}
 }
 
-func splitSemver(ver string) (int, int, int) {
+func splitSemver(ver string) Version {
 	ver = strings.TrimPrefix(ver, "v")
 	parts := strings.Split(ver, ".")
 
-	return mustAtoi(parts[0]), mustAtoi(parts[1]), mustAtoi(parts[2])
+	return Version{
+		Major: mustAtoi(parts[0]),
+		Minor: mustAtoi(parts[1]),
+		Patch: mustAtoi(parts[2]),
+	}
 
 }
 
